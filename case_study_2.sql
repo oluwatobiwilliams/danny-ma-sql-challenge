62b0bacfc4bf3fbcb4d8b27ec19df27c0e3eccc7
Servus <30422857+oluwatobiwilliams@users.noreply.github.com>
2022-08-30 01:30:46 +0100

    Add answer A.8 - A.10

--- case_study_2.sql
+++ case_study_2.sql
@@ -1,16 +1,16 @@
 /*
 A. Pizza Metrics
 
-How many pizzas were ordered?
-How many unique customer orders were made?
-How many successful orders were delivered by each runner?
-How many of each type of pizza was delivered?
-How many Vegetarian and Meatlovers were ordered by each customer?
-What was the maximum number of pizzas delivered in a single order?
-For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
-How many pizzas were delivered that had both exclusions and extras?
-What was the total volume of pizzas ordered for each hour of the day?
-What was the volume of orders for each day of the week?
+1. How many pizzas were ordered?
+2. How many unique customer orders were made?
+3. How many successful orders were delivered by each runner?
+4. How many of each type of pizza was delivered?
+5. How many Vegetarian and Meatlovers were ordered by each customer?
+6. What was the maximum number of pizzas delivered in a single order?
+7. For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+8. How many pizzas were delivered that had both exclusions and extras?
+9. What was the total volume of pizzas ordered for each hour of the day?
+10. What was the volume of orders for each day of the week?
 */
 
 -- A.1
@@ -94,8 +94,61 @@ COUNT(CASE WHEN change = 0 THEN 0 END) AS pizza_no_change,
 COUNT(CASE WHEN change = 1 THEN 1 END) AS pizza_with_change
 FROM (
   	SELECT c.*,
-		CASE WHEN exclusions IS NULL AND extras IS NULL THEN 0 ELSE 1 END AS change
-	FROM cleaned_orders AS c
-	WHERE order_id IN (SELECT order_id FROM runner_orders WHERE cancellation = 0)
+		  CASE WHEN exclusions IS NULL AND extras IS NULL THEN 0 ELSE 1 END AS change
+	  FROM cleaned_orders AS c
+	  WHERE order_id IN (SELECT order_id FROM runner_orders WHERE cancellation = 0)
 ) AS pizza_changes
 GROUP BY 1;
+
+-- A.8
+WITH cleaned_orders AS (
+  SELECT order_id, customer_id, pizza_id,
+  CASE WHEN exclusions IN ('null','') THEN null ELSE exclusions END AS exclusions,
+  CASE WHEN extras IN ('null','') THEN null ELSE extras END AS extras,
+  order_time
+  FROM pizza_runner.customer_orders
+),
+runner_orders AS (
+  SELECT order_id, runner_id, pickup_time, distance, duration,
+  CASE WHEN cancellation IN ('null', '') OR cancellation IS NULL THEN 0
+  	ELSE 1 END AS cancellation
+  FROM pizza_runner.runner_orders
+)
+SELECT COUNT(*) AS pizza_delivered 
+FROM (
+  SELECT c.*,
+  CASE WHEN exclusions IS NOT NULL AND extras IS NOT NULL 
+      THEN 1 ELSE 0 END AS both_ex
+  FROM cleaned_orders AS c
+  WHERE order_id IN (SELECT order_id FROM runner_orders WHERE cancellation = 0)
+ ) AS check_both_ex
+ WHERE both_ex = 1;
+
+ -- A.9
+SELECT EXTRACT(hour FROM order_time) AS hod,
+COUNT(*) AS pizza_ordered
+FROM pizza_runner.customer_orders
+GROUP BY hod
+ORDER BY pizza_ordered DESC;
+
+-- A.10
+SELECT TO_CHAR(order_time,'Day') AS dow,
+EXTRACT(dow FROM order_time) AS dow_2,
+COUNT(*) AS pizza_ordered
+FROM pizza_runner.customer_orders
+GROUP BY 1, 2
+ORDER BY pizza_ordered DESC;
+
+/*
+B. Runner and Customer Experience
+
+1. How many runners signed up for each 1 week period? (i.e. week starts 2021-01-01)
+2. What was the average time in minutes it took for each runner to arrive at the Pizza Runner HQ to pickup the order?
+3. Is there any relationship between the number of pizzas and how long the order takes to prepare?
+4. What was the average distance travelled for each customer?
+5. What was the difference between the longest and shortest delivery times for all orders?
+6. What was the average speed for each runner for each delivery and do you notice any trend for these values?
+7. What is the successful delivery percentage for each runner?
+*/
+
+-- B.1
