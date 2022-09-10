eee892132c49db6091cd2697005e1771c8ddd163
Servus <30422857+oluwatobiwilliams@users.noreply.github.com>
2022-09-11 00:33:22 +0100

    Update B.3

--- case_study_4.sql
+++ case_study_4.sql
@@ -51,5 +51,44 @@ ROUND(PERCENTILE_CONT(0.95) WITHIN GROUP (ORDER BY node_days)::NUMERIC,1) AS per
 FROM customer_nodes
 GROUP BY region_id;
 
+/*
+B. Customer Transactions
+1. What is the unique count and total amount for each transaction type?
+2. What is the average total historical deposit counts and amounts for all customers?
+3. For each month - how many Data Bank customers make more than 1 deposit and either 1 purchase or 1 withdrawal in a single month?
+4. What is the closing balance for each customer at the end of the month?
+5. What is the percentage of customers who increase their closing balance by more than 5%?
+*/
 
+-- B.1
+SELECT txn_type, COUNT(*) AS no_of_transactions, SUM(txn_amount) AS total_amount 
+FROM data_bank.customer_transactions
+GROUP BY txn_type;
 
+-- B.2
+SELECT ROUND(AVG(no_of_transactions),1) AS avg_deposit_txn_count,
+ROUND(AVG(total_amount),1) AS avg_total_deposit
+FROM (
+  SELECT customer_id, 
+  COUNT(*) AS no_of_transactions, 
+  SUM(txn_amount) AS total_amount 
+  FROM data_bank.customer_transactions
+  WHERE txn_type = 'deposit'
+  GROUP BY customer_id
+) AS tmp
+;
+
+-- B.3
+SELECT txn_month,
+COUNT(CASE WHEN deposit_count>1 AND (withdrawal_count>1 OR purchase_count>1) THEN customer_id END) AS no_of_customers
+FROM (
+  SELECT customer_id, 
+  DATE_TRUNC('month', txn_date) AS txn_month,
+  COUNT(CASE WHEN txn_type = 'deposit' THEN 1 END) AS deposit_count,
+  COUNT(CASE WHEN txn_type = 'withdrawal' THEN 1 END) AS withdrawal_count,
+  COUNT(CASE WHEN txn_type = 'purchase' THEN 1 END) AS purchase_count
+  FROM data_bank.customer_transactions
+  GROUP BY customer_id, txn_month
+) AS tmp
+GROUP BY txn_month ORDER BY txn_month
+;
