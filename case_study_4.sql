36ceb9e991e3ff86cb7b09a3eecdbee9a0abc2d9
Servus <30422857+oluwatobiwilliams@users.noreply.github.com>
2022-09-13 00:31:11 +0100

    Update B.5

--- case_study_4.sql
+++ case_study_4.sql
@@ -113,33 +113,39 @@ ORDER BY customer_id, month_
 ;
 
 --B.5
-WITH percentage_cte AS
-(
-SELECT *,
-SUM(txn_amount) OVER (PARTITION BY customer_id ORDER BY month_ ASC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS balance
-
-FROM
-(SELECT customer_id,DATE_TRUNC('Month',txn_date) AS month_, SUM(txn_group)AS txn_amount 
-FROM
-  (SELECT *,
-  CASE WHEN txn_type = 'deposit' THEN txn_amount
-  ELSE -1 * txn_amount END AS txn_group
-  FROM data_bank.Customer_Transactions
-  ORDER BY customer_id, txn_date
-  ) AS tmp1
-GROUP BY 1,2
-ORDER BY 1,2 
-)AS tmp2
-ORDER BY customer_id, month_ 
+WITH customer_balance AS (
+  SELECT *,
+  SUM(txn_amount) OVER (PARTITION BY customer_id ORDER BY month_ ASC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS balance
+  FROM (
+    SELECT customer_id,DATE_TRUNC('Month',txn_date) AS month_, SUM(txn_group)AS txn_amount 
+    FROM (
+      SELECT *,
+      CASE WHEN txn_type = 'deposit' THEN txn_amount
+        ELSE -1 * txn_amount END AS txn_group
+        FROM data_bank.Customer_Transactions
+        ORDER BY customer_id, txn_date
+    ) AS update_txn_amount
+    GROUP BY 1,2
+  ) AS monthly_totals
+),
+growth_rates AS (
+  SELECT customer_id, month_, balance, previous_month_balance, 
+  CASE WHEN previous_month_balance IS NULL THEN NULL
+  		WHEN previous_month_balance = 0 THEN balance*100
+  ELSE ROUND(((balance-(previous_month_balance))/ABS(previous_month_balance))*100,1) END AS growth_rate,
+  ROW_NUMBER () OVER (PARTITION BY customer_id ORDER BY month_ DESC) AS balance_index
+  FROM (
+      SELECT *,
+      LAG(balance) OVER (PARTITION BY customer_id ORDER BY month_) AS previous_month_balance
+      FROM customer_balance
+      ) AS add_previous_month_balance
+),
+cust_last_balance AS (
+  SELECT customer_id, month_, growth_rate,
+  CASE WHEN growth_rate > 5 THEN 1 ELSE 0 END AS growth_rate_check 
+  FROM growth_rates
+  WHERE balance_index = 1
 )
 
-SELECT customer_id, month_, balance, previous_month_balance, 
-
-CASE WHEN previous_month_balance IS NULL THEN NULL
-ELSE ROUND((balance/NULLIF(previous_month_balance,0))-1,1)*100 END AS growth
-FROM 
- (SELECT *,
-  LAG(balance) OVER (PARTITION BY customer_id ORDER BY month_) AS previous_month_balance
-  FROM percentage_cte
- )AS tmp
-;
+SELECT (SUM(growth_rate_check)/COUNT(*)::FLOAT)*100
+FROM cust_last_balance;
