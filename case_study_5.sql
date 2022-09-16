3a74aedd4d8b3b2b4a6b0f696bb49408b573cea0
Servus <30422857+oluwatobiwilliams@users.noreply.github.com>
2022-09-16 01:15:49 +0100

    Update questions to section 1

--- case_study_5.sql
+++ case_study_5.sql
@@ -1,28 +1,30 @@
 /*
 1. Data Cleansing Steps
+
 In a single query, perform the following operations and generate a new table in the data_mart schema named clean_weekly_sales:
+1. Convert the week_date to a DATE format
 
-Convert the week_date to a DATE format
+2. Add a week_number as the second column for each week_date value, for example any value from the 1st of January to 7th of January will be 1, 8th to 14th will be 2 etc
 
-Add a week_number as the second column for each week_date value, for example any value from the 1st of January to 7th of January will be 1, 8th to 14th will be 2 etc
+3. Add a month_number with the calendar month for each week_date value as the 3rd column
 
-Add a month_number with the calendar month for each week_date value as the 3rd column
+4. Add a calendar_year column as the 4th column containing either 2018, 2019 or 2020 values
 
-Add a calendar_year column as the 4th column containing either 2018, 2019 or 2020 values
-
-Add a new column called age_band after the original segment column using the following mapping on the number inside the segment value
+5. Add a new column called age_band after the original segment column using the following mapping on the number inside the segment value
 
 segment	age_band
 1	Young Adults
 2	Middle Aged
 3 or 4	Retirees
-Add a new demographic column using the following mapping for the first letter in the segment values:
+
+6. Add a new demographic column using the following mapping for the first letter in the segment values:
 segment	demographic
 C	Couples
 F	Families
-Ensure all null string values with an "unknown" string value in the original segment column as well as the new age_band and demographic columns
 
-Generate a new avg_transaction column as the sales value divided by transactions rounded to 2 decimal places for each record
+7. Ensure all null string values with an "unknown" string value in the original segment column as well as the new age_band and demographic columns
+
+8. Generate a new avg_transaction column as the sales value divided by transactions rounded to 2 decimal places for each record
 */
 
 -- 1
