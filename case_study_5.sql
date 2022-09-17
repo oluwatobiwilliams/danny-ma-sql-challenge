3da83198f0c201f9496b3b8d3102bf5feb227f7b
Servus <30422857+oluwatobiwilliams@users.noreply.github.com>
2022-09-18 00:29:09 +0100

    Complete section 2

--- case_study_5.sql
+++ case_study_5.sql
@@ -58,3 +58,113 @@ FROM (
 	FROM datamart_weekly_sales 
 ) AS clean_weekdate;
 
+/*
+2. Data Exploration
+
+a. What day of the week is used for each week_date value?
+b. What range of week numbers are missing from the dataset?
+c. How many total transactions were there for each year in the dataset?
+d. What is the total sales for each region for each month?
+e. What is the total count of transactions for each platform
+f. What is the percentage of sales for Retail vs Shopify for each month?
+g. What is the percentage of sales by demographic for each year in the dataset?
+h. Which age_band and demographic values contribute the most to Retail sales?
+i. Can we use the avg_transaction column to find the average transaction size for each year for Retail vs Shopify? If not - how would you calculate it instead?
+*/
+
+-- 2.a
+SELECT TO_CHAR(week_date, 'day') AS day_of_week
+FROM cleaned_weekly_sales 
+LIMIT 1;
+
+-- 2.b
+WITH date_range AS (
+	SELECT MIN(week_date) AS first_date,
+	MAX(week_date) AS last_date
+	FROM cleaned_weekly_sales
+),
+
+week_date_series AS (
+	SELECT GENERATE_SERIES(first_date, last_date, '1 week') AS week_date
+	FROM date_range
+)
+SELECT * 
+FROM week_date_series
+WHERE week_date NOT IN (SELECT DISTINCT week_date FROM cleaned_weekly_sales)
+ORDER BY week_date;
+
+-- 2.c
+SELECT EXTRACT (YEAR FROM week_date) AS year, SUM(transactions) AS total_transactions 
+FROM cleaned_weekly_sales cws 
+GROUP BY year ORDER BY year;
+
+-- 2.d
+SELECT region, 
+DATE_TRUNC('month', week_date) AS month,
+SUM(sales) AS total_sales
+FROM cleaned_weekly_sales
+GROUP BY 1,2 ORDER BY 1,2;
+
+-- 2.e
+SELECT platform, 
+SUM(transactions) AS total_transactions
+FROM cleaned_weekly_sales
+GROUP BY 1;
+
+-- 2.f
+SELECT *,
+ROUND(shopify_sales/total_sales::NUMERIC,2) AS shopify_percent,
+ROUND(retail_sales/total_sales::NUMERIC,2) AS retail_percent
+FROM (
+	SELECT 
+	DATE_TRUNC('month', week_date) AS month,
+	SUM(CASE WHEN platform = 'Shopify' THEN sales END) AS shopify_sales,
+	SUM(CASE WHEN platform = 'Retail' THEN sales END) AS retail_sales,
+	SUM(sales) AS total_sales
+	FROM cleaned_weekly_sales
+	GROUP BY 1 ORDER BY 1
+) AS sales_split
+;
+
+-- 2.g
+SELECT *,
+ROUND(couples_sales/total_sales::NUMERIC,2) AS couples_percent,
+ROUND(family_sales/total_sales::NUMERIC,2) AS family_percent,
+ROUND(unknown_sales/total_sales::NUMERIC,2) AS unknown_percent
+FROM (
+	SELECT 
+	EXTRACT(YEAR FROM week_date) AS year,
+	SUM(CASE WHEN demographic = 'Couples' THEN sales END) AS couples_sales,
+	SUM(CASE WHEN demographic = 'Families' THEN sales END) AS family_sales,
+	SUM(CASE WHEN demographic = 'unknown' THEN sales END) AS unknown_sales,
+	SUM(sales) AS total_sales
+	FROM cleaned_weekly_sales
+	GROUP BY 1 ORDER BY 1
+) AS sales_split
+;
+
+-- 2.h
+SELECT CONCAT(demographic, '-', age_band) AS demographic_age,
+SUM(sales) AS total_sales,
+RANK() OVER (ORDER BY SUM(sales) DESC) AS most_contribution
+FROM cleaned_weekly_sales
+WHERE platform = 'Retail'
+GROUP BY 1 ORDER BY 2 DESC;
+
+-- 2.i
+-- We can't use the avg_transaction column as that would be averaging an averaged value
+-- which is misleading
+SELECT *,
+shopify_sales/shopify_txn AS shopify_avg_transaction,
+retail_sales/retail_txn AS retail_avg_transaction
+FROM (
+	SELECT EXTRACT (YEAR FROM week_date) AS year,
+	SUM(CASE WHEN platform = 'Shopify' THEN sales END) AS shopify_sales,
+	SUM(CASE WHEN platform = 'Shopify' THEN transactions END) AS shopify_txn,
+	SUM(CASE WHEN platform = 'Retail' THEN sales END) AS retail_sales,
+	SUM(CASE WHEN platform = 'Retail' THEN transactions END) AS retail_txn
+	FROM cleaned_weekly_sales 
+	GROUP BY year 
+) AS sales_split
+;
+
